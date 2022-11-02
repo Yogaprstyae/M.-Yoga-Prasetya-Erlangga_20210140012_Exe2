4c2edc505526ce5e74ac2644f52b34a3fa49f115
Yogaprstyae <m.yoga.ft21@mail.umy.ac.id>
2022-11-02 15:50:36 +0700

    Add method input

--- M. Yoga Prasetya Erlangga_20210140012_Exe2/Program.cs
+++ M. Yoga Prasetya Erlangga_20210140012_Exe2/Program.cs
@@ -12,6 +12,29 @@ namespace M.Yoga_Prasetya_Erlangga_20210140012_Exe2
         int n;
         int i;
 
+        public void input()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter the number of elements in yeh array : ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 52))
+                    break;
+                else
+                    Console.WriteLine("Array maksimum 52 number of data");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("----------------------");
+            Console.WriteLine(" Enter Array Elements ");
+            Console.WriteLine("----------------------");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                yoga[i] = Int32.Parse(s1);
+            }
+        }
         static void Main(string[] args)
         {
         }
