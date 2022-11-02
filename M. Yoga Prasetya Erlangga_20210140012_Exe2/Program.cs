95d3e6871c8338b3d2bee2d986787a608881109f
Yogaprstyae <m.yoga.ft21@mail.umy.ac.id>
2022-11-02 15:26:10 +0700

    Add project files.

---
+++ M. Yoga Prasetya Erlangga_20210140012_Exe2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M.Yoga_Prasetya_Erlangga_20210140012_Exe2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
